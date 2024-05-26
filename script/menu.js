e7dfda47f543a429abcb64f2947a0b0d7afb25dc
Joel Canté <97580350+JoelCante@users.noreply.github.com>
2024-05-25 22:49:28 -0600

    Add files via upload

--- script/menu.js
+++ script/menu.js
@@ -7,3 +7,20 @@ window.addEventListener("scroll", function(){
     var menu = document.getElementById('ancla');
     menu.classList.toggle("ancla",window.scrollY>50);
   })  
+
+  $(document).ready(function(){
+    $('.subir').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 1000);
+    });
+  });
+
+  function toggle(element) {
+    element.classList.toggle("cambio");
+  }
+
+
+  function abrir() {
+    $("#menu").slideToggle();
+}
